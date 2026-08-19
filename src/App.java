f0751c58939abdbeaadf139433d12763fb48ee9f
One-Nine-Zero <jvargas130307@gmail.com>
2026-08-18 19:37:01 -0500

    submiting App

---
+++ src/App.java
@@ -0,0 +1,7 @@
+//Programa haciendo uso del metodo main para ejecutar el print "Hello, World!" en el Terminal
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println((double) 2 * 4/6);
+    }
+}
---
+++ src/MiClase.java
@@ -0,0 +1,12 @@
+public class MiClase {
+    private int edad = 1000000;
+    private double altura = 1.00000000000000001;
+    private char sexo = 'M';
+    private boolean aporvada = false;
+    private String nombre = "Juan David";
+    private String saludo = "Hola, " + nombre;
+    private double nota;
+        nota = 4.5;
+    private var nota1 = 5;
+    private int aproximaciónAltura = (int) altura;
+}
